5052b96a24487d86c51647f01be689a2efd79c3f
Huriel Lopes <huriellopes1996@gmail.com>
2020-03-26 22:13:50 -0300

    First Commit - Frontend Be the hero

---
+++ README.md
@@ -0,0 +1,14 @@
+# Semana Omnistack 11.0
+
+> Aplicação frontend feita em Reactjs da Be The Hero!
+
+### Para o desenvolvimento foram usadas as tecnologias:
+
+    - Reactjs
+    - Axios
+    - React Icons
+    - React Routes Dom
+
+### Créditos
+
+- RocketSeat
---
+++ src/pages/Logon/index.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
+
+import './styles.css';
+import logoImg from '../../assets/logo.svg';
+import herosImg from '../../assets/heroes.png';
+
+function Logon() {
+    const [id, setId] = useState('');
+
+    const history = useHistory();
+
+    async function handlelogin(e) {
+        e.preventDefault();
+
+        try {
+            const resp = await api.post('sessions', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', resp.data.name);
+
+            history.push('/profile');
+        } catch (err) {
+            alert('Falha no Login, tente novamente!');
+        }
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form onSubmit={handlelogin}>
+                    <h1>Faça seu logon</h1>
+                    <input 
+                        placeholder="Sua ID" 
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
+                    <button type="submit" className="button">Entrar</button>
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#E02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+
+            <img src={herosImg} alt="Heros"/>
+        </div>
+    );
+}
+
+export default Logon;
