b712a303ea49afd584235378e4fa90b91c66dd59
Huriel Lopes <huriellopes1996@gmail.com>
2020-03-27 17:23:35 -0300

    teste

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const localUrl = 'http://localhost:3333'
+const localUrl = 'http://localhost:3333';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || localUrl
+    baseURL: process.env.REACT_APP_API_URL || localUrl,
 });
 
 export default api;
